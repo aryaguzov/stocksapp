10383f47ec0ee2fccb89b37e9978f9d895fcacfe
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-05-25 21:08:18 +0300

    Initial commit

---
+++ src/main/java/com/ar/stocksapp/SimpleStocksApplication.java
@@ -0,0 +1,13 @@
+package com.ar.stocksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStocksApplication.class, args);
+	}
+
+}
