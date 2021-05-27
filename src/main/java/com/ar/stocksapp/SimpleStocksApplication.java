8dfc12b65f2efa90c09337ec5f5d9963564687bb
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-05-27 23:22:27 +0300

    Add a RestTemplate bean.

--- src/main/java/com/ar/stocksapp/SimpleStocksApplication.java
+++ src/main/java/com/ar/stocksapp/SimpleStocksApplication.java
@@ -2,12 +2,18 @@ package com.ar.stocksapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SimpleStocksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleStocksApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleStocksApplication.class, args);
-	}
-
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
